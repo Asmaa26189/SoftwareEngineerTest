6f0de11960b7c04ccbea27ae9051b061a4955d7d
Asmaa Hesham <108579670+Asmaa26189@users.noreply.github.com>
2023-05-04 01:20:46 +0300

    Software Engineer Test

---
+++ SoftwareTest/Controllers/DriverController.cs
@@ -0,0 +1,169 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SoftwareTest.Models;
+using SoftwareTest.DatabaseCURD;
+using Microsoft.Extensions.Configuration;
+using System.Data.SqlClient;
+using SoftwareTest.DatabaseCURD;
+using System.Diagnostics;
+
+namespace SoftwareTest.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class DriverController : ControllerBase
+    {
+        private IConfiguration Configuration;
+        /**
+         * Rest API control to acess all database operations 
+         */
+        public DriverController(IConfiguration _configuration)
+        {
+            Configuration = _configuration;
+        }
+        /**
+         * InsertDriver - insert Driver in db
+         * 
+         * @driver : driver want to be insert
+         * 
+         * Return : json result of driver id
+         */
+        [HttpPost]
+        public JsonResult InsertDriver(Driver driver)
+        {
+            DriverCRUD driverCRUD = new DriverCRUD(Configuration);
+            return new JsonResult(Ok(driverCRUD.InsertDriver(driver)));
+        }
+        /**
+         * UpdateDriver - update driver in database
+         * 
+         * @driver : driver want to be update
+         * 
+         * Return : json
+         */
+        [HttpPost]
+        public JsonResult UpdateDriver(Driver driver)
+        {
+            DriverCRUD driverCRUD = new DriverCRUD(Configuration);
+            return new JsonResult(Ok(driverCRUD.UpdateDriver(driver)));
+        }
+        /**
+         * DeleteDriver - delete driver from database
+         * 
+         * @id : id of driver want to be deleted
+         * 
+         * Return : json
+         * 
+         */
+        [HttpDelete]
+        public JsonResult DeleteDriver(int id)
+        {
+            DriverCRUD driverCRUD = new DriverCRUD(Configuration);
+            return new JsonResult(Ok(driverCRUD.DeleteDriver(id)));
+        }
+        /**
+         * SelectAllDrivers - select all drivers from database
+         * 
+         * Return :json of drivers 
+         */
+        [HttpGet("/SelectAllDrivers")]
+        public JsonResult SelectAllDrivers()
+        {
+            DriverCRUD driverCRUD = new DriverCRUD(Configuration);
+            return new JsonResult(Ok(driverCRUD.SelectAllDrivers()));
+        }
+        /**
+         * SelectDriver - select driver with id
+         *
+         *@id : id of driver 
+         *
+         *Return :json of selected driver
+         *
+         */
+        [HttpGet]
+        public JsonResult SelectDriver(int id)
+        {
+            DriverCRUD driverCRUD = new DriverCRUD(Configuration);
+            return new JsonResult(Ok(driverCRUD.SelectDriver(id)));
+        }
+        /**
+         * InsertDriverRandom100 - insert random 100 Driver name in db
+         * 
+         * @driver : driver want to be insert
+         * 
+         * Return : json result of driver id
+         */
+        [HttpPost]
+        public JsonResult InsertDriverRandom100()
+        {
+            string charsUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            string charLower =   "abcdefjhklmnopqrstwvxyz";
+            string numbers = "0123456789";
+
+            for (int i = 0; i < 100; i++)
+            {
+
+
+                var random = new Random();
+                string randomStringUpperF = new string(Enumerable.Repeat(charsUpper, 1)
+                                                        .Select(s => s[random.Next(s.Length)]).ToArray());
+                string randomStringLowerF = new string(Enumerable.Repeat(charLower, 4)
+                                                        .Select(s => s[random.Next(s.Length)]).ToArray());
+                string firstName = randomStringUpperF + randomStringLowerF;
+                string randomStringUpperS = new string(Enumerable.Repeat(charsUpper, 1)
+                                                        .Select(s => s[random.Next(s.Length)]).ToArray());
+                string randomStringLowerS = new string(Enumerable.Repeat(charLower, 4)
+                                                        .Select(s => s[random.Next(s.Length)]).ToArray());
+                string secondName = randomStringUpperS + randomStringLowerS;
+
+                string phoneNumber = new string(Enumerable.Repeat(numbers, 9)
+                                                        .Select(s => s[random.Next(s.Length)]).ToArray());
+
+                Driver driver = new Driver();
+                driver.FirstName = firstName;
+                driver.LastName = secondName;
+                driver.Email = firstName+secondName+"@email.com";
+                driver.PhoneNumber = "+2"+ phoneNumber;
+
+
+                DriverCRUD driverCRUD = new DriverCRUD(Configuration);
+                driverCRUD.InsertDriver(driver);
+            }
+            
+                return new JsonResult(Ok());
+        }
+
+        /**
+        * SelectAllDrivers - select all Alphabetized drivers Full Name from database
+        * 
+        * Return :json of Alphabetized drivers Names
+        */
+        [HttpGet("/SelectAllDriversAlphabetized")]
+        public JsonResult SelectAllDriversAlphabetized()
+        {
+            DriverCRUD driverCRUD = new DriverCRUD(Configuration);
+            List<Driver> drivers = new List<Driver>();
+            List<string> newDrivers = new List<string>();
+                for (int i = 0; i < driverCRUD.SelectAllDrivers().Count; i++)
+                {
+                    Driver driver = driverCRUD.SelectAllDrivers()[i];
+                    newDrivers.Add(String.Concat(driver.FirstName.OrderBy(ch => ch)).Trim() +" " +  String.Concat(driver.LastName.OrderBy(ch => ch)).Trim());
+                }
+
+            return new JsonResult(Ok(newDrivers));
+        }
+
+        /**
+        * SelectAllDriversOrderes - select all drivers ordered from database
+        * 
+        * Return :json of ordered drivers 
+        */
+        [HttpGet("/SelectAllDriversOrdered")]
+        public JsonResult SelectAllDriversOrdered()
+        {
+            DriverCRUD driverCRUD = new DriverCRUD(Configuration);    
+            return new JsonResult(Ok(driverCRUD.SelectAllDrivers().OrderBy(p => p.FirstName).ThenBy(p => p.LastName).ToList()));
+        }
+ 
+    }
+}
---
+++ SoftwareTest/DatabaseCURD/DriverCRUD.cs
@@ -0,0 +1,211 @@
+﻿using System.Data.SqlClient;
+using Microsoft.AspNetCore.Mvc;
+using SoftwareTest.Models;
+using Microsoft.Extensions.Configuration;
+namespace SoftwareTest.DatabaseCURD
+{ 
+    /**
+     * Class for Database Driver CRUD operations 
+     */
+    public class DriverCRUD
+    {
+    private IConfiguration _configuration;
+        public DriverCRUD(IConfiguration Configuration)
+        {
+            _configuration = Configuration;
+        }
+        /**
+         * SelectAllDrivers - select all drivers in database
+         * 
+         * Return : List of drivers
+         *
+         */
+        public List<Driver> SelectAllDrivers()
+        {
+            List<Driver> drivers = new List<Driver>();
+            string conStr = _configuration.GetConnectionString("DefaultConnection");
+            string query = "SELECT * FROM Drivers";
+            using (SqlConnection con = new SqlConnection(conStr))
+            {
+                using (SqlCommand cmd = new SqlCommand(query))
+                {
+                    cmd.Connection = con;
+                    con.Open();
+                    using (SqlDataReader sdr = cmd.ExecuteReader())
+                    {
+                        while (sdr.Read())
+                        {
+                            drivers.Add(new Driver
+                            {
+                                Id = Convert.ToInt32(sdr["Id"]),
+                                FirstName = Convert.ToString(sdr["FirstName"]),
+                                LastName = Convert.ToString(sdr["LastName"]),
+                                Email = Convert.ToString(sdr["Email"]),
+                                PhoneNumber = Convert.ToString(sdr["PhoneNumber"])
+                            });
+                        }
+                    }
+                    con.Close();
+                }
+            }
+
+            if (drivers.Count == 0)
+            {
+                drivers.Add(new Driver());
+            }
+            return drivers;
+
+        }
+
+        /**
+         * InsertDriver - insert driver in database
+         * 
+         * @driver : driver want to insert
+         * 
+         * Return : id of driver
+         *
+         */
+        public Driver InsertDriver(Driver driver)
+        {
+            string query = "INSERT INTO Drivers (FirstName,LastName,Email,PhoneNumber) VALUES (@FirstName,@LastName,@Email,@PhoneNumber)";
+            query += "SELECT SCOPE_IDENTITY()";
+            string conStr = _configuration.GetConnectionString("DefaultConnection");
+
+            using (SqlConnection con = new SqlConnection(conStr))
+            {
+                using (SqlCommand cmd = new SqlCommand(query))
+                {
+                    cmd.Connection = con;
+                    cmd.Parameters.AddWithValue("@FirstName", driver.FirstName.Trim());
+                    cmd.Parameters.AddWithValue("@LastName", driver.LastName.Trim());
+                    cmd.Parameters.AddWithValue("@Email", driver.Email.Trim());
+                    cmd.Parameters.AddWithValue("@PhoneNumber", driver.PhoneNumber.Trim());
+                    con.Open();
+                    driver.Id = Convert.ToInt32(cmd.ExecuteScalar());
+                    con.Close();
+                }
+            }
+
+            return driver;
+        }
+
+        /**
+          * UpdateDriver - update driver in database
+          * 
+          * @driver : driver want to update
+          * 
+          * Return : boolean true
+          *
+          */
+        public Boolean UpdateDriver(Driver driver)
+        {
+            string query = "UPDATE Drivers SET FirstName=@FirstName,LastName=@LastName,Email=@Email,PhoneNumber= @PhoneNumber WHERE Id=@Id";
+            string conStr = _configuration.GetConnectionString("DefaultConnection");
+
+            using (SqlConnection con = new SqlConnection(conStr))
+            {
+                using (SqlCommand cmd = new SqlCommand(query))
+                {
+                    cmd.Connection = con;
+                    cmd.Parameters.AddWithValue("@Id", driver.Id);
+                    if (driver.FirstName != null)
+                        cmd.Parameters.AddWithValue("@FirstName", driver.FirstName.Trim());
+                    else
+                        cmd.Parameters.AddWithValue("@FirstName", "");
+                    if (driver.LastName != null)
+                        cmd.Parameters.AddWithValue("@LastName", driver.LastName.Trim());
+                    else
+                        cmd.Parameters.AddWithValue("@LastName", "");
+                    if (driver.Email != null)
+                        cmd.Parameters.AddWithValue("@Email", driver.Email.Trim());
+                    else
+                        cmd.Parameters.AddWithValue("@Email", "");
+                    if (driver.PhoneNumber != null)
+                        cmd.Parameters.AddWithValue("@PhoneNumber", driver.PhoneNumber.Trim());
+                    else
+                        cmd.Parameters.AddWithValue("@PhoneNumber", "");
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+                    con.Close();
+                }
+            }
+
+            return true;
+        }
+        /**
+          * DeleteDriver - delete driver from database
+          * 
+          * @Id : driver id want to delete
+          * 
+          * Return : boolean true
+          *
+          */
+        public Boolean DeleteDriver(int Id)
+        {
+            string query = "DELETE FROM Drivers WHERE Id=@Id";
+            string conStr = _configuration.GetConnectionString("DefaultConnection");
+            using (SqlConnection con = new SqlConnection(conStr))
+            {
+                using (SqlCommand cmd = new SqlCommand(query))
+                {
+                    cmd.Connection = con;
+                    cmd.Parameters.AddWithValue("@Id", Id);
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+                    con.Close();
+                }
+            }
+
+            return true;
+        }
+        /**
+          * SelectDriver - select driver from database
+          * 
+          * @Id : driver id want to select
+          * 
+          * Return : list of drivers (just one driver return according to id)
+          *
+          */
+        public List<Driver> SelectDriver(int Id)
+        {
+            List<Driver> drivers = new List<Driver>();
+            string query = "Select * FROM Drivers WHERE Id=@Id";
+            string conStr = _configuration.GetConnectionString("DefaultConnection");
+            using (SqlConnection con = new SqlConnection(conStr))
+            {
+                using (SqlCommand cmd = new SqlCommand(query))
+                {
+                    cmd.Connection = con;
+                    cmd.Parameters.AddWithValue("@Id", Id);
+                    con.Open();
+                    using (SqlDataReader sdr = cmd.ExecuteReader())
+                    {
+                        while (sdr.Read())
+                        {
+                            drivers.Add(new Driver
+                            {
+                                Id = Convert.ToInt32(sdr["Id"]),
+                                FirstName = Convert.ToString(sdr["FirstName"]).Trim(),
+                                LastName = Convert.ToString(sdr["LastName"]).Trim(),
+                                Email = Convert.ToString(sdr["Email"]).Trim(),
+                                PhoneNumber = Convert.ToString(sdr["PhoneNumber"]).Trim()
+                            });
+                        }
+                    }
+                    con.Close();
+                }
+
+            }
+
+            if (drivers.Count == 0)
+            {
+                drivers.Add(new Driver());
+            }
+            return drivers;
+        }
+
+    }
+}
+
+
+
